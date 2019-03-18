ee989dd34d62e32cf6f6f44fb1914daefa6326db
Flor Barbosa <esthelabarbosa@gmail.com>
2019-03-17 22:41:15 -0600

    Delete credentials.js

--- lab5-callbacks/credentials.js
+++
@@ -1,4 +0,0 @@
-
-module.exports.MAPBOX_TOKEN = 'pk.eyJ1IjoiZmxvcmVzdGhlbGEiLCJhIjoiY2p0MXRtZXBnMGNwMjQzcGd2cTl4bHowcCJ9.nDypu15Q6txjYr6wU3CVOg'
-
-module.exports.DARK_SKY_SECRET_KEY = '89e7e478ecc70e4b461d7d134a7a68cd'
